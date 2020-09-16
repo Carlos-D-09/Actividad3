ed67140db682aa444c3c9164881c8741af9b8d33
Carlos M <carlos.medina1311@alumnos.udg.mx>
2020-09-16 17:38:24 -0500

    Pedimos el nombre, lo leemos con fgets e imprimimos un saludo

--- Actividad3.c
+++ Actividad3.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 
 int main (){
+    char nombre[100];
     printf("\nHello World");
+    printf("\n\nIntroduce tu nombre: ");
+    fgets(nombre, sizeof(nombre),stdin);
+    printf("\nHola: %s", nombre);
+
+    printf("\n");
     return 0;
 }
