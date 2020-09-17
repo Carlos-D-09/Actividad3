554540241e8d17abbc39250ff36f1d0d876b09af
Carlos M <carlos.medina1311@alumnos.udg.mx>
2020-09-16 19:23:51 -0500

    Pedimos la edad, la capturamos e imprimimos el proximo cumpleaños

--- Actividad3.c
+++ Actividad3.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 
 int main (){
+    unsigned int edad;
     char nombre[100];
-    printf("\nHello World");
+    printf("Hello World");
     printf("\n\nIntroduce tu nombre: ");
     fgets(nombre, sizeof(nombre),stdin);
     printf("\nHola: %s", nombre);
-
-    printf("\n");
+    printf("\nCuál es la edad: ");
+    scanf("\n%d", &edad);
+    printf("\nVas a cumplir %d", edad+1);
+    printf("\n\n");
     return 0;
 }
